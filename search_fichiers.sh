2ad98419966db9298b747932c0a6e658779ef275
Alexia123 <alexia.zs.16@gmail.com>
2019-10-28 13:53:15 +0100

    Deuxieme commit - Trouver des fichiers

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
 echo "Bienvenu $USERNAME, nous sommes le $(date +%m) $(date +%B) $(date +%Y)"
-read -p "Quel répertoire vous intéresse aujourd'hui ?
+read -p "Quel répertoire (chemin absolu) vous intéresse aujourd'hui ?
 " chemin
 echo "Vous avez entré :
 $chemin"
+find ${chemin} -name $1
