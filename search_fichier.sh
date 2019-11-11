77f4a7fb8c59c28b802a71293f8c6aa905b548bf
enaihali <amal.ilahiane@gmail.com>
2019-11-11 17:02:19 +0100

    first commit

---
+++ search_fichier.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Bonjour $USER bienvenu dans ce programme, nous sommes le $(date)"
+echo "Quel répertoire vous intéresse ?"
+read -p 'Inserer le chemin du répertoire' chemin 
+echo "$chemin"
