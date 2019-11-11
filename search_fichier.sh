856119c54d3720aaed42892b9cb0bc76aa1ecc1e
enaihali <amal.ilahiane@gmail.com>
2019-11-11 17:39:14 +0100

    ajout de la recherche par pattern

--- search_fichier.sh
+++ search_fichier.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "Bonjour $USER bienvenu dans ce programme, nous sommes le $(date)"
+
 echo "Quel répertoire vous intéresse ?"
 read -p 'Inserer le chemin du répertoire' chemin 
 echo "$chemin"
+
+ls $1
+
