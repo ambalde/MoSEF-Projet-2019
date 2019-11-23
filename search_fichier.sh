92507865737e4a8b53ec7c20ff16eef261b43fa8
Scaroubelek <oscar.vidal@live.fr>
2019-11-23 15:45:02 +0100

    deuxieme commit

--- search_fichier.sh
+++ search_fichier.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
 #Execution et message de bienvenue
 date=$(date | cut -d "," -f1)
+
 echo "Bonjour $USER, nous sommes le $date"
+
 read -p "Quel répertoire vous intéresse aujourd'hui ?" chemin
-echo "$chemin"
+
+echo "Le repertoire choisi est : $chemin"
+#On affiche tous les fichiersrépertoire qui respectent le pattern entré en params
+ls $chemin/$1
+#Affiche les fichiers selon le deuxieme pattern
+
+grep -il  $2 $repertoire/$1
 
