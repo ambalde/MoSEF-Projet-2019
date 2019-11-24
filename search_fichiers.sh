e5c9488f9a708f6ebab8537c81c0cad51b24e4b3
Haidara <missiliou.haidara@gmail.com>
2019-11-24 20:07:42 +0100

    mis à jour pour la question 7

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -6,3 +6,6 @@ echo "Bienvenu $USERNAME, nous sommes le" $la_date_du_jour
 #Demander à l'utilisateur de saisir un repertoire puis on l'affiche avec la fonction echo
 read -p "Quel repertoire vous intérersse aujourd'hui ? " nom_repertoire
 echo $nom_repertoire
+
+#Afficher tous les fichiers dont le nom respecte un pattern fourni en paramètre
+ls $nom_repertoire/$1
