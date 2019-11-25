5916499714e6d6409135bb63cb7b2fc08c48f86f
DonovanThaing <donovan.thaing@hotmail.fr>
2019-11-25 16:06:50 +0100

    Modif script : recherche fichiers par pattern

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -2,4 +2,4 @@
 
 echo "Bonjour "$USER", nous sommes le $(date +%A) $(date +%d) $(date +%B)."
 read -p "Quel répertoire vous intéresse aujourd'hui ?" chemin;
-ls $chemin
+find $chemin -maxdepth 1 -iname "$1"
